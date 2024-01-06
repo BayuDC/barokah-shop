634fda683695ab9f3a23ee7b601e4da87f29e1ba
BayuDC <dev.bayudc@gmail.com>
2024-01-06 13:55:14 +0700

    chore: add icons

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <Html>
+    <Head>
+      <Title>Toko Barokah</Title>
+      <Link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" />
+    </Head>
     <Body class="bg-black text-white">
       <NuxtLayout>
         <NuxtPage />
--- components/Header.vue
+++ components/Header.vue
@@ -2,11 +2,16 @@
 
 <template>
   <header class="bg-primary/10 border-primary border-b-2 px-5 lg:px-10">
-    <div class="container max-w-screen-lg mx-auto grid items-center grid-cols-3 h-20">
-      <h1 class="text-2xl font-bold leading-5">Toko Barokah</h1>
-      <div></div>
-      <div class="ml-auto">
-        <div class="h-12 w-12 bg-white rounded-full"></div>
+    <div class="container max-w-screen-lg mx-auto grid items-center grid-cols-2 h-20">
+      <h1 class="text-2xl font-bold leading-6">Toko Barokah</h1>
+      <div class="ml-auto flex items-center">
+        <div class="mr-2 lg:mr-4">
+          <i class="bx bx-search bx-md block"></i>
+        </div>
+        <div class="mr-4 lg:mr-6">
+          <i class="bx bx-cart bx-md block"></i>
+        </div>
+        <div class="h-10 w-10 bg-white rounded-full"></div>
       </div>
     </div>
   </header>
