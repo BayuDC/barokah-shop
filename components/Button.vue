22b577fc9ff381b077c0d0429c16ace7d09f0170
BayuDC <dev.bayudc@gmail.com>
2024-01-31 13:10:11 +0700

    feat: go to login page

--- components/Button.vue
+++ components/Button.vue
@@ -1,7 +1,14 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const emit = defineEmits<{
+  click: [];
+}>();
+</script>
 
 <template>
-  <button class="text-black bg-primary block font-medium py-2 md:py-2.5 rounded-md text-sm md:text-base">
+  <button
+    @click="emit('click')"
+    class="text-black bg-primary block font-semibold px-5 py-2 md:py-2.5 rounded-md text-sm md:text-base"
+  >
     <slot />
   </button>
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'empty',
+});
+
+const config = useRuntimeConfig();
+
+function goToLogin(service: string) {
+  navigateTo(`${config.public.apiUrl}/auth/${service}`, {
+    external: true,
+  });
+}
+</script>
+
+<template>
+  <div class="h-screen w-screen flex justify-center items-center px-3">
+    <div class="bg-primary/10 border-primary border-2 rounded-xl container max-w-96 p-5">
+      <h1 class="text-2xl py-2 lg:text-3xl font-bold leading-6 text-center">Toko Barokah</h1>
+      <div class="mt-6 flex flex-col gap-4">
+        <Button
+          @click="goToLogin('google')"
+          class="!bg-[#F2F2F2] !text-[#1F1F1F] flex items-center justify-between gap-2"
+        >
+          <span>Masuk dengan Google</span>
+          <i class="bx bxl-google bx-sm"></i>
+        </Button>
+        <Button @click="goToLogin('discord')" class="!bg-[#5865F2] text-white flex items-center justify-between gap-2">
+          <span>Masuk dengan Discord</span>
+          <i class="bx bxl-discord-alt bx-sm"></i>
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
