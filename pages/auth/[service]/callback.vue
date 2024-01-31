ee1cbc87a55bf1745e3948dfc180c54bdd4c195a
BayuDC <dev.bayudc@gmail.com>
2024-01-31 13:34:40 +0700

    feat: redirect after login

---
+++ pages/auth/[service]/callback.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'empty',
+});
+
+const route = useRoute();
+const token = useLocalStorage('token', '');
+
+const { data, error } = await useMyFetch<{
+  token: string;
+}>(route.path, {
+  query: route.query,
+  timeout: 5000,
+});
+
+onMounted(() => {
+  if (error.value) {
+    navigateTo('/login');
+  } else {
+    token.value = data.value?.token;
+    navigateTo('/');
+  }
+});
+</script>
+
+<template>
+  <h1 class="py-10 px-4 text-center text-2xl font-bold text-primary">Tunggu sebentar ya...</h1>
+</template>
+
+<style scoped></style>
